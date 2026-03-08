a543e5daf9438bcc2f3fd76f3fb87131c9882928
ivanm151 <ivmedv5@gmail.com>
2026-03-08 23:08:21 +0700

    fix

--- server/api.js
+++ server/api.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Корневой маршрут — для проверки
+
 app.get('/', (req, res) => {
     res.send('Прокси-сервер работает. Используйте POST /api/detect');
 });
